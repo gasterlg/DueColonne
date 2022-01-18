2471d4d2c1724ae81a129df6481a2d9d3aaf631c
Luca Grilli <luca@lucagrilli.com>
2022-01-18 23:17:54 +0800

    Initial Commit

---
+++ DueColonne/DueColonneApp.swift
@@ -0,0 +1,17 @@
+//
+//  DueColonneApp.swift
+//  DueColonne
+//
+//  Created by Luca on 1/18/22.
+//
+
+import SwiftUI
+
+@main
+struct DueColonneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
